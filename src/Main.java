d0a19f98e9057104dd4704bb3b605f90988e492c
maviva97@gmail.com <maviva97@gmail.com>
2023-10-09 12:23:04 +0300

    hh

--- src/Main.java
+++ src/Main.java
@@ -3,16 +3,23 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
 
-        int counter =100;
-        for (int i = 0; i < 10; i++) {
-            for (int j = 0; j < 10; j++) {
-                if (counter%5==0||counter%6==0)
-                {System.out.print(counter);
-                System.out.print(" "); }// I think it is best to have spaces between the numbers
-                counter=counter + 10;
-                       if (counter >= 1000) break;}
-            //after printing 10 numbers, go to a new line
-            System.out.println();
+            int i = 0;
+
+            for (int number = 100; number <= 1000; number ++){
+
+                if (number % 5 == 0 && number % 6 == 0) {
+                    System.out.print(number + " ");
+                    i ++;
+
+                    if (i % 10 == 0) {
+                        System.out.println();
+                    }
+                }
+            }
         }
     }
-}
+
+
+
+
+
